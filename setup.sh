2b0531cc7493b3ebf4129382c951cb6c02e19531
Giacomo Marciani <mgiacomo@amazon.com>
2019-05-08 20:36:55 +0200

    Bugifixed setup scripts

--- setup.sh
+++ setup.sh
@@ -7,15 +7,17 @@ OPERATION=$1
 OBJECT=$2
 
 HOME_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+CONTROLLERS_DIR="${HOME_DIR}/kubernetes/controllers"
 
 METACONTROLLER_NAMESPACE=metacontroller
 METACONTROLLER_RBAC=https://raw.githubusercontent.com/GoogleCloudPlatform/metacontroller/master/manifests/metacontroller-rbac.yaml
 METACONTROLLER_YAML=https://raw.githubusercontent.com/GoogleCloudPlatform/metacontroller/master/manifests/metacontroller.yaml
 
-SMARTSCALER_CONTROLLER_YAML="${HOME_DIR}/controllers/smartscaler-controller.yaml"
-SMARTSCALER_LOGIC_PY="${HOME_DIR}/controllers/hooks/smartscaler-hooks-logic.py"
-SMARTSCALER_HOOK_DEPLOYMENT_YAML="${HOME_DIR}/controllers/hooks/smartscaler-hook-deployment.yaml"
-SMARTSCALER_HOOK_SERVICE_YAML="${HOME_DIR}/controllers/hooks/smartscaler-hook-service.yaml"
+SMARTSCALER_CRD_YAML="${CONTROLLERS_DIR}/smartscaler-crd.yaml"
+SMARTSCALER_CONTROLLER_YAML="${CONTROLLERS_DIR}/smartscaler-controller.yaml"
+SMARTSCALER_LOGIC_PY="${CONTROLLERS_DIR}/hooks/smartscaler-hooks-logic.py"
+SMARTSCALER_HOOK_DEPLOYMENT_YAML="${CONTROLLERS_DIR}/hooks/smartscaler-hook-deployment.yaml"
+SMARTSCALER_HOOK_SERVICE_YAML="${CONTROLLERS_DIR}/hooks/smartscaler-hook-service.yaml"
 
 SMARTSCALER_SERVICE_NAME="smartscaler-controller"
 SMARTSCALER_DEPLOYMENT_NAME="smartscaler-controller"
@@ -23,7 +25,7 @@ SMARTSCALER_CONFIGMAP_NAME="smartscaler-controller"
 SMARTSCALER_COMPOSITECONTROLLER_NAME="smartscaler-controller"
 
 function printUsage {
-    echo "bash setup.sh [install|uninstall] [metacontroller|controller]"
+    echo "bash setup.sh [install|uninstall] [metacontroller|smartscaler]"
 }
 
 function installMetacontroller {
@@ -37,20 +39,22 @@ function uninstallMetacontroller {
     echo "Uninstalling Metacontroller"
 }
 
-function installController {
+function installSmartScalerController {
     echo "Installing Smart Scaler Controller"
+    kubectl apply -f ${SMARTSCALER_CRD_YAML}
     kubectl apply -f ${SMARTSCALER_CONTROLLER_YAML}
     kubectl create configmap ${SMARTSCALER_CONFIGMAP_NAME} --from-file=${SMARTSCALER_LOGIC_PY}
     kubectl apply -f ${SMARTSCALER_HOOK_DEPLOYMENT_YAML}
     kubectl apply -f ${SMARTSCALER_HOOK_SERVICE_YAML}
 }
 
-function uninstallController {
+function uninstallSmartScalerController {
     echo "Uninstalling Smart Scaler Controller"
     kubectl delete service ${SMARTSCALER_SERVICE_NAME}
     kubectl delete deployment ${SMARTSCALER_DEPLOYMENT_NAME}
     kubectl delete configmap ${SMARTSCALER_CONFIGMAP_NAME}
     kubectl delete compositecontroller ${SMARTSCALER_COMPOSITECONTROLLER_NAME}
+    kubectl delete -f ${SMARTSCALER_CRD_YAML}
 }
 
 if [[ -z ${OPERATION} ]] | [[ -z ${OBJECT} ]]; then
@@ -61,14 +65,14 @@ fi
 if [[ ${OPERATION} = "install" ]]; then
     if [[ ${OBJECT} = "metacontroller" ]]; then
         installMetacontroller
-    elif [[ ${OBJECT} = "controller" ]]; then
-        installController
+    elif [[ ${OBJECT} = "smartscaler" ]]; then
+        installSmartScalerController
     fi
 elif [[ ${OPERATION} = "uninstall" ]]; then
     if [[ ${OBJECT} = "metacontroller" ]]; then
         uninstallMetacontroller
-    elif [[ ${OBJECT} = "controller" ]]; then
-        uninstallController
+    elif [[ ${OBJECT} = "smartscaler" ]]; then
+        uninstallSmartScalerController
     fi
 fi
 
